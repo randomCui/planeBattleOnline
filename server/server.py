5186ddacb80f15a429acac460c1d195529167f9f
randomCui <czy_scu@qq.com>
2022-05-14 11:06:02 +0800

    fix the problems which server process can't terminate

--- server/server.py
+++ server/server.py
@@ -1,20 +1,17 @@
 import pickle
 import socket
 import threading
-import pygame
-
-import game_logic
+import time
 
 from base.game import Game
 from base.player import Player
 from base.texture import Texture
-from client.config import window_height, ip, port
+from base.config import window_height, ip, port
 
 t = Texture()
 
 
 def client_thread(connection, game_id, player_id):
-
     player_attr = pickle.loads(connection.recv(2048))
     player_attr['basic_setting']['x'] = 100
     player_attr['basic_setting']['y'] = window_height - 100
@@ -42,14 +39,12 @@ def client_thread(connection, game_id, player_id):
             del current_game.players[player_id]
             connection.close()
             break
-        except Exception as e:
-            print(e)
 
     print("Lost connection")
     print("game %s has %d players" % (game_id,len(current_game.players)))
     print(current_game.players)
-    try:
 
+    try:
         if len(current_game.players) == 0:
             del games[game_id]
             print("Closing Game", game_id)
@@ -69,22 +64,12 @@ def init_server(server, port):
     return s
 
 
-if __name__ == '__main__':
-    server = ip
-    port = port
+def acceptation_thread():
+    service_threading = []
 
-    s = init_server(server, port)
+    s = init_server(ip, port)
     print("Waiting for a connection, Server Started")
-
-    # 服务器的所有网络连接
-    connected = set()
-
-    # 服务器正在运行的所有游戏房间字典
-    games = {}
-
-    idCount = 0
-    game_id = 0
-
+    global game_id
     while True:
         conn, address = s.accept()
         print("Connected to:", address)
@@ -94,4 +79,28 @@ if __name__ == '__main__':
             games[game_id] = Game(game_id)
             print("Creating a new game...")
         print("Adding player %s in game %s" % (address[1], game_id))
-        threading.Thread(target=client_thread, args=(conn, game_id, address[1])).start()
+
+        temp = threading.Thread(target=client_thread, args=(conn, game_id, address[1]))
+        temp.setDaemon(True)
+        temp.start()
+        service_threading.append(temp)
+
+
+if __name__ == '__main__':
+    idCount = 0
+    game_id = 0
+
+    # 服务器的所有网络连接
+    connected = set()
+
+    # 服务器正在运行的所有游戏房间字典
+    games = {}
+
+    listen_thread = threading.Thread(target=acceptation_thread)
+    listen_thread.setDaemon(True)
+    listen_thread.start()
+
+    time.sleep(0.3)
+    while True:
+        if input('>> ') == 'q':
+            break
